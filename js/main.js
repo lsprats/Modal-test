4d6bda77de40f1a35852e4e3282326feb0b52dd5
lsprats <lsp.prats@gmail.com>
2015-03-12 23:59:56 -0300

    modal-first-preview

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+function mostrarModal() {
+  var ancho = 300;
+  var alto=300;
+  var x = (screen.width / 2)-(ancho / 2);
+  var y = (screen.height / 2) - (alto / 2);
+  modal.style.left = x + "px";
+  modal.style.top = y + "px";
+  modal.style.visibility = "visible";
+}
